57a6ad6b1cbe37f318254a852f6af79974ff7be0
Laiane Suzart <laiane@pm.me>
2025-03-16 18:32:05 -0300

    feat: display character card on champion selection

--- src/components/Card3d/index.jsx
+++ src/components/Card3d/index.jsx
@@ -1,11 +1,22 @@
 import { houseColors } from '../../styles/utils';
 import './style.css';
 
-function Card3d({ char }) {
+function Card3d({ char, onClick }) {
   const { image, name, house = '' } = char;
 
   return (
-    <div className="card3d" style={{ borderColor: houseColors[house.toLowerCase()] }}>
+    <div
+      className="card3d"
+      style={{ borderColor: houseColors[house.toLowerCase()] }}
+      tabIndex={0}
+      onClick={onClick}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          onClick();
+        }
+      }}
+      role="button"
+    >
       <div className="houseBox">
         <span style={{ color: houseColors[house.toLowerCase()] }}>{house}</span>
       </div>
--- src/pages/Champions/index.jsx
+++ src/pages/Champions/index.jsx
@@ -2,10 +2,22 @@ import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { LuRefreshCcw } from 'react-icons/lu';
 
+import Gryffindor from '../../assets/img/gryffindor.svg?react';
+import Slytherin from '../../assets/img/slytherin.svg?react';
+import Hufflepuff from '../../assets/img/hufflepuff.svg?react';
+import Ravenclaw from '../../assets/img/ravenclaw.svg?react';
+
 import { getStudents, staleTime } from '../../services/api';
 import { useLoading, useModal } from '../../store';
 import Card3d from '../../components/Card3d';
 import './style.css';
+
+const crests = {
+  gryffindor: <Gryffindor />,
+  slytherin: <Slytherin />,
+  hufflepuff: <Hufflepuff />,
+  ravenclaw: <Ravenclaw />,
+};
 
 function Champions() {
   const {
@@ -19,6 +31,7 @@ function Champions() {
   });
 
   const [selected, setSelected] = useState([]);
+  const [highlightedCard, setHighlightedCard] = useState(null);
   const setLoading = useLoading((state) => state.setLoading);
   const openModal = useModal((state) => state.openModal);
 
@@ -39,6 +52,7 @@ function Champions() {
 
   const selectAgain = () => {
     setSelected(getParticipants(data));
+    window.scrollTo(0, 0);
   };
 
   useEffect(() => {
@@ -54,21 +68,52 @@ function Champions() {
   }, [data]);
 
   return (
-    <main className="container-flex">
-      <h2>Choose your champion</h2>
-      <div>
-        <div className="card-container">
-          {selected.map((char) => (
-            <Card3d char={char} key={char.id} />
-          ))}
+    <>
+      {highlightedCard ? (
+        <div className="clip-wrapper">
+          <div className="overlay">
+            {crests[highlightedCard.house?.toLowerCase()]}
+            <div className="info-card">
+              <figure>
+                <img src={highlightedCard.image} alt={highlightedCard.name} />
+                <figcaption>{highlightedCard.name}</figcaption>
+              </figure>
+              <div>
+                <p>Birth: {highlightedCard.dateOfBirth || '-'}</p>
+                <p>Species: {highlightedCard.species || '-'}</p>
+                <p>Patronus: {highlightedCard.patronus || '-'}</p>
+              </div>
+              <button
+                onClick={() => setHighlightedCard(null)}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    setHighlightedCard(null);
+                  }
+                }}
+              >
+                close
+              </button>
+            </div>
+          </div>
         </div>
-        {data.length ? (
-          <button onClick={selectAgain} className="btn--refresh" title="Refresh options">
-            <LuRefreshCcw />
-          </button>
-        ) : null}
-      </div>
-    </main>
+      ) : (
+        <main className="container-flex">
+          <h2>Choose your champion</h2>
+          <div>
+            <div className="card-container">
+              {selected.map((char) => (
+                <Card3d char={char} key={char.id} onClick={() => setHighlightedCard(char)} />
+              ))}
+            </div>
+            {data.length ? (
+              <button onClick={selectAgain} className="btn--refresh" title="Refresh options">
+                <LuRefreshCcw />
+              </button>
+            ) : null}
+          </div>
+        </main>
+      )}
+    </>
   );
 }
 
